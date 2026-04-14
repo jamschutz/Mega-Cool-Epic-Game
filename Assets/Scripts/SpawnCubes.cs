f73da8a3f9c992b558cc133e5d2ac50d90be7a7c
joey schutz <schutzjoey@gmail.com>
2026-04-14 11:17:10 -0400

    spawn cubes

---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    
+    public void OnSpawn(InputAction.CallbackContext context)
+    {
+        if(context.started) {
+            for(int i = 0; i < 1000; i++) {
+                Vector3 pos = new(0, Random.Range(1, 2), Random.Range(-0.5f, 1.5f));
+                GameObject.Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
+    }
+}
